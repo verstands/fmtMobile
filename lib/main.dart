e4370be1ad5291852f579edbe70c8be62cf737c2
Rabby <rabbymbamu@gmail.com>
2022-07-25 13:18:34 +0200

    correction de fichier main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'FM',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
--- lib/screens/load.dart
+++ lib/screens/load.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/material.dart';
-import 'package:fmt/screens/home.dart';
+import 'package:fmt/constant.dart';
+import 'package:fmt/models/api_response.dart';
+import 'package:fmt/screens/accueil.dart';
+import 'package:fmt/screens/login.dart';
+import 'package:fmt/services/login.service.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -16,16 +21,16 @@ class _LoadingState extends State<Loading> {
     String token = await getToken();
     if (token == '') {
       Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (context) => login()), (route) => false);
+          MaterialPageRoute(builder: (context) => Login()), (route) => false);
     } else {
       ApiResponse response = await getUserDetail();
       if (response.erreur == null) {
         Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => accueil()),
+            MaterialPageRoute(builder: (context) => Accueil()),
             (route) => false);
       } else if (response.erreur == unauthorized) {
         Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => login()), (route) => false);
+            MaterialPageRoute(builder: (context) => Login()), (route) => false);
       } else {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text('${response.erreur}'),
